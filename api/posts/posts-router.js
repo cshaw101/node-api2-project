20514b0c60ef008e4b6c7e54bd126479f2541edf
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-23 22:53:25 -0400

    post/id is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,7 +16,21 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }else {
+            res.status(200).json(post)
+        }
 
+    } catch (err){
+        res.status(500).json({message: "The post information could not be retrieved" })
+    }
+})
 
 
 module.exports = router;
