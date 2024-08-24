7bf6e2f04731ce3a92d9aecbed43043336d186cb
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-23 23:37:06 -0400

    completed

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,16 +5,33 @@ const Posts  = require('./posts-model');
 
 const router = express.Router()
 
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        }
+
+        const comments = await Posts.findPostComments(req.params.id);
+        res.status(200).json(comments)
+
+    }catch (err) {
+            res.status(500).json({
+                message: "The comments information could not be retrieved"
+            })
+    }
+})
+
 router.get('/', async (req, res) => {
     try {
         const posts = await Posts.find();
-        res.status(200).json(posts)
+        res.status(200).json(posts);
     } catch (err) {
-        res.status(500).json({
-            message: "The posts information could not be retrieved" 
-        })
+        res.status(500).json({ message: "The posts information could not be retrieved" });
     }
-})
+});
 
 router.get('/:id', async (req, res) => {
     try {
@@ -97,6 +114,8 @@ router.delete('/:id', async (req, res) => {
         });
     }
 });
+
+
 
 
 module.exports = router;
